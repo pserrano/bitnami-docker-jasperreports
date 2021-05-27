54f6009aa1001bd86dbe63264100bbf5a77c5659
Pablo Serrano <info@pabloserrano.net>
2021-05-27 15:41:21 +0200

    adding patch docker aws

--- 7/debian-10/rootfs/app-entrypoint.sh
+++ 7/debian-10/rootfs/app-entrypoint.sh
@@ -8,6 +8,11 @@ print_welcome_page
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/init.sh" ]]; then
     nami_initialize tomcat mysql-client jasperreports
     info "Starting gosu... "
+    if [ -d "/etc/opt/bitnami/jasperreports/config/" ]; then
+        if ! [ -z "$(ls -A /etc/opt/bitnami/jasperreports/config/)" ]; then 
+            cp -r "/etc/opt/bitnami/jasperreports/config/"* "/opt/bitnami/jasperreports/"
+        fi
+    fi
 fi
 
 exec tini -- "$@"
